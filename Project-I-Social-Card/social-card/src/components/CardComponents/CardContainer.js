76963fde3dea7c2a65c8ef61a6c6cc2eb5264e4f
Timothy Shores <timothymshores@gmail.com>
2019-02-11 19:37:14 -0600

    Imported HeaderContainer and CardContainer into App.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+function CardContainer() {
+    return <h3>Card Container</h3>;
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContainer() {
+    return <h3>Header Container</h3>;
+}
+
+export default HeaderContainer;
