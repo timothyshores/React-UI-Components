de49072d15b6df280efc25f352d2ea3748fa2378
Timothy Shores <timothymshores@gmail.com>
2019-03-18 17:55:13 -0500

    Completed MVP

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
+import ImageThumnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <div className="header">
+            <div className="avatar">
+                <ImageThumnail />
+            </div>
+            <div className="avatar">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderContainer;
