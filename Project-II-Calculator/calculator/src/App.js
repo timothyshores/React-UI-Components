a4d90db018356f6569b8bce1346e630d919dde36
Timothy Shores <timothymshores@gmail.com>
2019-03-19 17:43:41 -0500

    Create and style OperatorButton component. Completed MVP

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import OperatorButton from './components/ButtonComponents/OperatorButton';
 
 const App = () => {
     return (
         <div className="app">
             <CalculatorDisplay />
-            <ActionButton button="clear" />
-            <NumberButton />
-            <ActionButton button="0" />
+            <div className="buttons">
+                <div className="white-buttons">
+                    <ActionButton button="clear" />
+                    <NumberButton />
+                    <ActionButton button="0" />
+                </div>
+                <div className="red-buttons">
+                    <OperatorButton />
+                </div>
+            </div>
         </div>
     );
 };
